e5e4cbbea9b2a710f0129ba15f0a4323d20ffba4
ClaudioJr17 <161088266+ClaudioJr17@users.noreply.github.com>
2024-11-01 10:36:03 -0300

    Add files via upload

---
+++ CLAUDIO/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'Java é uma linguagem multiplataforma, orientada a objetos e centrada em rede que pode ser usada como uma plataforma em si.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Latim',
+    'Como se diz Oi em Latim?',
+    'Oi em latim é Heus'
+)
